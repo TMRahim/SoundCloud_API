954835ce865a5aee874f4d49620e85bb67927b1c
tmrahim <t.manawer@mybihs.org>
2015-12-17 22:57:58 +0000

    enter

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
+SC.initialize({
+  client_id: '5f3cfea1b1b2cb888459afe8b3c070d2'
+});
+
 $(document).ready(function(){
+  
   $("#text").click(function(event){
-    console.log()
-    
     SC.get('/tracks', { q: $( "#info" ).val(), license: 'cc-by-sa' }, function(tracks) {
       for (var i=0; i<10; i++){
         var title = tracks[i].title;
         var url = tracks[i].permalink_url;
         var genre = tracks[i].genre;
-        $("#middle").append(title + ", " + url + ", " + genre);
+        $("#middle").append(title + ", " + url + ", " + genre + " " + ('<input type="button" class="play" value="test">'));
         $("#middle").append("<br>");
       }
+    });
   });
   
+
+  $("#info").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#text").click();
+    }
   });
-  SC.initialize({
-  client_id: '5f3cfea1b1b2cb888459afe8b3c070d2'
-  });
+  
   
   $("#clear").click(function(){
     $("#middle").empty();
